9098fa34646622ef9cc0f27e946cfb9480314c16
Thiago Navarro <thiago@oxyoy.com>
2021-09-02 19:24:33 +0000

    init

---
+++ htmlAntiCopy.nimble
@@ -0,0 +1,21 @@
+# Package
+
+version       = "0.1.0"
+author        = "Thiago Navarro"
+description   = "Block copy of any text in HTML"
+license       = "MIT"
+srcDir        = "src"
+
+binDir = "build"
+bin = @["htmlAntiCopy"]
+
+
+# Dependencies
+
+requires "nim >= 1.5.1"
+
+task buildRelease, "Build the release version":
+  exec "nimble -d:release build"
+task buildJs, "Build the release js version":
+  switch("backend", "js")
+  exec "nim js -d:release --out:build/ src/htmlAntiCopy.nim"
---
+++ src/htmlAntiCopy.nim
@@ -0,0 +1,58 @@
+#[
+  Created at: 09/02/2021 17:20:13 Thursday
+  Modified at: 09/02/2021 07:23:55 PM Thursday
+
+        Copyright (C) 2021 Thiago Navarro
+  See file "license" for details about copyright
+]#
+
+from std/random import sample, rand, randomize
+from std/strutils import IdentChars, Letters
+from std/strformat import fmt
+
+const
+  textElement = "span"
+  charElement = "i"
+
+type
+  ShuffledText* = tuple
+    text, class, css: string
+
+func genCss(class: string, fakeCharsIndex: openArray[int]): string =
+  for i in fakeCharsIndex:
+    result.add fmt"{textElement}.{class} {charElement}:nth-child({i}),"
+  if result[^1] == ',':
+    result = result[0..^2]
+  result.add "{font-size:0px;}"
+  result.add fmt"{textElement}.{class} {charElement}"
+  result.add "{font-style: unset;}"
+
+randomize()
+
+const FakeChars = IdentChars + {' '}
+
+proc shuffle*(text: string): ShuffledText =
+  var
+    fakeWordsIndex: seq[int]
+  for _ in 0..rand 10..20:
+    result.class.add sample IdentChars
+  while result.class[0] notin Letters:
+    result.class = result.class[1..^1]
+  for ch in text:
+    result.text.add ch
+    while rand(0..1) > 0:
+      result.text.add sample FakeChars
+      fakeWordsIndex.add result.text.len
+  result.css = result.class.genCss fakeWordsIndex
+
+func toHtml*(self: ShuffledText): string =
+  var text = ""
+  for ch in self.text:
+    text.add fmt"<{charElement}>{ch}</{charElement}>"
+  result = fmt"""<style>{self.css}</style><{textElement} class="{self.class}">{text}</{textElement}>"""
+
+proc antiCopy*(text: cstring): cstring {.exportc.} =
+  shuffle($text).toHtml.cstring
+
+when isMainModule:
+  echo "Hello World".shuffle.toHtml
